10e4d3c9f067fd67933098e502e736530f5e1f9b
wondong <tonykang4715@gmail.com>
2024-01-22 22:15:49 +0900

    today's workflow for slam

--- SLAM/wd_pyslam/extractor.py
+++ SLAM/wd_pyslam/extractor.py
@@ -1,8 +1,10 @@
 import cv2
 import numpy as np
 
+from scipy.spatial import cKDTree
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform, EssentialMatrixTransform
+from helpers import add_ones, poseRt, fundamentalToRt, normalize,  myjet #EssentialMatrixTransform,
 
 # turn [[x,y]] -> [[x,y,1]]
 def add_ones(x):
@@ -29,29 +31,27 @@ class FeatureExtractor(object):
     def denormalize(self, pt):
         
         ret =  np.dot(self.K, np.array([pt[0], pt[1], 1]))
-        
+        ret /= ret[2]
         return int(round(ret[0])), int(round(ret[1]))
         # return int(round(pt[0] + self.w // 2)), int(round(pt[1] + self.h // 2))
-    def extractRt(self, E):
-                W = np.mat([[0, -1, 0], 
-                [1, 0, 0], 
-                [0, 0, 1]], dtype = float)
+    def extractRt(self, E):        
+        W = np.mat([[0, -1, 0], 
+        [1, 0, 0], 
+        [0, 0, 1]], dtype = float)
 
-                u, w, vt = np.linalg.svd(E)
-                if np.linalg.det(u) < 0:
-                    u *= -1.
-                if np.linalg.det(vt) < 0:
-                    vt *= -1.  
-                R = np.dot(np.dot(u, W), vt)
-                if np.sum(R.diagonal()) < 0:
-                    R = np.dot(np.dot(u, W.T), vt) 
-                t = u[:, 2].reshape(3, 1)
-
-                print(R.shape)
-                print(t.shape)
-                pose = np.concatenate([R, t], axis = 1)
-                
-                return pose
+        # Lowe's ratio test 
+        u, w, vt = np.linalg.svd(E)
+        if np.linalg.det(u) < 0:
+            u *= -1.
+        if np.linalg.det(vt) < 0:
+            vt *= -1.  
+        R = np.dot(np.dot(u, W), vt)
+        if np.sum(R.diagonal()) < 0:
+            R = np.dot(np.dot(u, W.T), vt) 
+        t = u[:, 2].reshape(3, 1)
+        pose = np.concatenate([R, t], axis = 1)
+        
+        return pose
 
         
     def extract(self, img):
@@ -66,7 +66,6 @@ class FeatureExtractor(object):
         ret = []
         if self.last is not None:
             matches = self.bf.knnMatch(des, self.last["des"], k= 2)
-            # matches = zip([kps[m.queryIdx] for m in matches], [self.last['kps'][m.trainIdx] for m in matches])
             for m, n in matches:
                 if m.distance < 0.75 *n.distance:
                     kps1, kps2 = kps[m.queryIdx].pt, self.last['kps'][m.trainIdx].pt
@@ -74,7 +73,6 @@ class FeatureExtractor(object):
         # filtering
         pose = None
         if len(ret)> 0 :
-
             # normalization for fundamental matrix
             ret = np.array(ret)
             ret[:, 0, :] = self.normalize(ret[:,0, :])
@@ -85,18 +83,12 @@ class FeatureExtractor(object):
                                 # FundamentalMatrixTransform, 
                                 EssentialMatrixTransform,
                                 min_samples=8, 
-                                residual_threshold=0.06, 
+                                residual_threshold=0.5, 
                                 max_trials=100)
             ret = ret[inlier]
             
             # rotation matrix check logic
             pose = self.extractRt(model.params)
-
-            print("#"*50)
-            print("Rotation matrix : ")
-            print(np.sum(pose.diagonal()))
-            print(pose)
-            
 
             # Focal length estimation
             # s, v, d = np.linalg.svd(model.params)
@@ -104,9 +96,86 @@ class FeatureExtractor(object):
             # self.f_est_avg.append(f_est)
             # print(f"f estimate : {f_est}, f_median{np.median(self.f_est_avg)}")
             # print(f"f average value : {np.mean(self.f_est_avg)}")
-
             # print(f"SVD result : {v}")
 
 
         self.last = {"kps" : kps, "des" : des}
         return ret, pose
+
+
+def extractFeatures(img):
+  orb = cv2.ORB_create()
+  # detection
+  pts = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8), 3000, qualityLevel=0.01, minDistance=3)
+
+  # extraction
+  kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], size=20) for f in pts]
+  kps, des = orb.compute(img, kps)
+
+  # return pts and des
+  return np.array([(kp.pt[0], kp.pt[1]) for kp in kps]), des
+
+def match_frames(f1, f2):
+
+  bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+  matches = bf.knnMatch(f1.des, f2.des, k=2)
+
+  # Lowe's ratio test
+  ret = []
+  idx1, idx2 = [], []
+  idx1s, idx2s = set(), set()
+
+  for m,n in matches:
+    if m.distance < 0.75*n.distance:
+      p1 = f1.kps[m.queryIdx]
+      p2 = f2.kps[m.trainIdx]
+
+      # be within orb distance 32
+      if m.distance < 20:
+        # keep around indices
+        # TODO: refactor this to not be O(N^2)
+        if m.queryIdx not in idx1s and m.trainIdx not in idx2s:
+          idx1.append(m.queryIdx)
+          idx2.append(m.trainIdx)
+          idx1s.add(m.queryIdx)
+          idx2s.add(m.trainIdx)
+          ret.append((p1, p2))
+
+  # no duplicates
+  assert(len(set(idx1)) == len(idx1))
+  assert(len(set(idx2)) == len(idx2))
+
+  assert len(ret) >= 8
+  ret = np.array(ret)
+  idx1 = np.array(idx1)
+  idx2 = np.array(idx2)
+
+  # fit matrix
+  model, inliers = ransac((ret[:, 0], ret[:, 1]),
+                          EssentialMatrixTransform,
+                            min_samples=8, 
+                            residual_threshold=5, 
+                            max_trials=1000)
+  print("Matches:  %d -> %d -> %d -> %d" % (len(f1.des), len(matches), len(inliers), sum(inliers)))
+  return idx1[inliers], idx2[inliers], fundamentalToRt(model.params)
+
+
+class Frame(object):
+  def __init__(self, img, K, pose=np.eye(4), tid=None, verts=None):
+    self.K = np.array(K)
+    self.pose = np.array(pose)
+
+    if img is not None:
+      self.h, self.w = img.shape[0:2]
+      if verts is None:
+        self.kps, self.des = extractFeatures(img)
+      else:
+        assert len(verts) < 256
+        self.kps, self.des = verts, np.array(list(range(len(verts)))*32, np.uint8).reshape(32, len(verts)).T
+      self.pts = [None]*len(self.kps)
+    else:
+      # fill in later
+      self.h, self.w = 0, 0
+      self.kps, self.des, self.pts = None, None, None
+
+    # self.id = tid if tid is not None else mapp.add_frame(self)
--- SLAM/wd_pyslam/slam.py
+++ SLAM/wd_pyslam/slam.py
@@ -1,16 +1,124 @@
-import cv2
+import sys
+sys.path.append("/home/wondong/code/SLAM/wd_pyslam/build/g2opy/lib")
+sys.path.append("/home/wondong/code/SLAM/wd_pyslam/build/pangolin")
 
-from display import process_frame
+import g2o
+import pangolin
+
+import cv2
+import numpy as np
+
+from extractor import FeatureExtractor, Frame, match_frames
+from time import time
+
+# from display import process_frame
+
+W = 640
+H = 480
+
+# Camera intrinsic
+F =190 # median value
+# F =225 # mean value
+K = np.array([[F, 0, W//2], 
+              [0, F, H//2], 
+              [0, 0, 1]])
+
+fe = FeatureExtractor(K)
+
+
+
+IRt = np.eye(4)
+
+frames = []
+
+class Point(object):
+    def __init__(self, loc):
+        self.frames = []
+        self.location = loc
+        self.idx = []
+    def add_observation(self, frame, idx):
+        self.frames.append(frame)
+        self.idx.append(idx)
+        
+
+def triangulation(pose1, pose2, pts1, pts2):
+    # reject the without enough parallax and behind the camera
+    pts4d = cv2.triangulatePoints(pose1[:3], pose2[:3, :], pts1.T, pts2.T).T
+    good_pts4d = (np.abs(pts4d[:, 3]) > 0.005) & (pts4d[:, 2] > 0.0)
+    pts4d = pts4d[good_pts4d]
+
+    # homogenous coordinate
+    pts4d /= pts4d[:, 3:]    
+    return pts4d
+
+
+def process_frame(img):
+    img = cv2.resize(img, (W, H))
+    
+    frame = Frame(img, K)
+    frames.append(frame)    
+    if len(frames) <= 1:
+        return
+    kps1_idx, kps2_idx, Rt = match_frames(frames[-1], frames[-2])    
+    kps1 = frames[-1].kps[kps1_idx]
+    kps2 = frames[-2].kps[kps2_idx]
+    # Rt = Rt[:3, :]
+    # print(sum(Rt.diagonal()))
+    # print(frames[-1].pose.shape)
+    frames[-1].pose = np.dot(Rt, frames[-2].pose)
+    
+    # # reject the without enough parallax
+    # pts4d = cv2.triangulatePoints(IRt[:3], Rt[:3, :], kps1.T, kps2.T).T
+    # good_pts4d = np.abs(pts4d[:, 3]) > 0.005
+    # pts4d = pts4d[good_pts4d]
+    
+    # # homogenous coordinate
+    # pts4d /= pts4d[:, 3:]
+    # # reject the behind the camera
+    
+    # good_pts4d = pts4d[:, 2] > 0.0
+    # pts4d = pts4d[good_pts4d]
+    pts4d = triangulation(IRt, Rt, kps1, kps2)
+
+    for i, p in enumerate(pts4d):
+        pt = Point(p)
+        pt.add_observation(frames[-1], kps1_idx[i])
+        pt.add_observation(frames[-2], kps2_idx[i])
+
+    for p1, p2 in zip(kps1, kps2):
+        u1, v1 = int(p1[0]), int(p1[1])
+        u2, v2 = int(p2[0]), int(p2[1])
+
+        cv2.circle(img, (u1, v1), color = (0, 0, 255), radius=3)
+        cv2.line(img, (u1, v1), (u2, v2), color = (255, 0, 0))
+    cv2.imshow("image", img)
+
+    # pts, Rt = fe.extract(img)
+
+    # if Rt is None:
+    #     return
+    # for p1, p2 in pts:
+    #     u1, v1 = fe.denormalize(p1)
+    #     u2, v2 = fe.denormalize(p2)
+
+    #     cv2.circle(img, (u1, v1), color = (0, 0, 255), radius=3)
+    #     cv2.line(img, (u1, v1), (u2, v2), color = (255, 0, 0))
+    # cv2.imshow("image", img)
+
 
 if __name__ == "__main__":
     data_path = "/home/wondong/code/SLAM/wd_pyslam/data/front_undistorted_video.mp4"
     cap = cv2.VideoCapture(data_path)
+    
     while cap.isOpened():
-
+        start_time =time()
         ret, frame =  cap.read()
-
+        
         if ret == True:
             process_frame(frame)
+            end_time = time()
+
+            print(f"fps : {1 / (end_time - start_time)}")
 
 
             if cv2.waitKey(1) & 0xff == ord("q"):
