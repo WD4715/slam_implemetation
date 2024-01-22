7559e2346be7b6cec0d72c42ed439371a132ce2e
wondong <tonykang4715@gmail.com>
2024-01-22 13:53:57 +0900

    feature extraction refinement

--- SLAM/wd_pyslam/extractor.py
+++ SLAM/wd_pyslam/extractor.py
@@ -1,12 +1,15 @@
 import cv2
 import numpy as np
 
+from skimage.measure import ransac
+from skimage.transform import FundamentalMatrixTransform
+
 class FeatureExtractor(object):
     
     def __init__(self):
-        self.orb = cv2.ORB_create(10000)
+        self.orb = cv2.ORB_create(100)
         self.last = None
-        self.bf = cv2.BFMatcher()
+        self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
         
     def extract(self, img):
         # detection
@@ -17,8 +20,25 @@ class FeatureExtractor(object):
         kps, des = self.orb.compute(img, kps)
         
         # matching
-        matches = None
-        self.last = {"kps" : kps, "des" : des}
+        ret = []
         if self.last is not None:
-            matches = self.bf.match(des, self.last["des"])
-        return kps, des, matches
+            matches = self.bf.knnMatch(des, self.last["des"], k= 2)
+            # matches = zip([kps[m.queryIdx] for m in matches], [self.last['kps'][m.trainIdx] for m in matches])
+            for m, n in matches:
+                if m.distance < 0.75 *n.distance:
+                    kps1, kps2 = kps[m.queryIdx].pt, self.last['kps'][m.trainIdx].pt
+                    ret.append((kps1, kps2))
+        # filtering
+        if len(ret)> 0 :
+            ret = np.array(ret)
+            
+            print(ret.shape)
+            model, inlier = ransac((ret[:, 0], ret[:, 1]), 
+                                FundamentalMatrixTransform, 
+                                min_samples=8, 
+                                residual_threshold=1, 
+                                max_trials=100)
+            ret = ret[inlier]
+            print(f"inliers : {sum(inlier)}")
+        self.last = {"kps" : kps, "des" : des}
+        return ret
